8d620212d84dace3ee7eda2e94f9b21a15b4a354
sameerasad <ssameerkhan525@gmail.com>
2020-08-23 18:04:07 -0400

    un needed change

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="App"  >
+      <h1>Hello World   </h1>
       
       
     </div>
